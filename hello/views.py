8ae0de100bf8402bc06998a06cc5eaeb7c458b2d
Rahulsahu7389 <rahulsahuppd@gmail.com>
2024-10-26 13:07:54 +0530

    added login system

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from hello.models import LoggedIn
+
+MESSAGE_TAGS = {
+    'name':'ramam'
+}
 
 
 
@@ -10,7 +14,16 @@ def index(request):
     return HttpResponse('this is the index page')
 
 def logged(request):
-    return HttpResponse('this is the logged page')
+    global MESSAGE_TAGS
+    if request.method == 'POST':
+       name2 = request.POST.get('name')
+       password2 = request.POST.get('password')
+       for i in LoggedIn.objects.all():
+           if name2 == i.name and password2 == i.password:
+               MESSAGE_TAGS['name'] = name2
+            #    user = form.save()
+               return redirect('/home')
+    return render(request,'login1.html')
 
 def academy(request):
     return HttpResponse('this is the academy page')
@@ -19,9 +32,14 @@ def loggedOut(request):
     return HttpResponse('this is the loggedOut page')
 
 def signed(request):
-    # if request.method == 'POST':
-    #     name1 = request.POST.get('name')
-    #     pass1 = request.POST.get('password')
+    if request.method == 'POST':
+        name1 = request.POST.get('name')
+        pass1 = request.POST.get('password')
+        # name2 = request.POST.get('')
+        log1 = LoggedIn(name = name1,password = pass1,extra1 = "none")
+
+        log1.save()
+        return redirect('/')
         
     return render(request,'sign1.html')
 
@@ -33,4 +51,6 @@ def clubs(request):
 
 def sports(request):
     return HttpResponse('this is the sports page')
+
+
 
